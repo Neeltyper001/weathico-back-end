80e5bacd05724f987535fc351297c419ff4b85fe
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-09-23 20:18:19 +0530

    updated

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const dotenv = require('dotenv').config();
+const api_key = process.env.API_KEY;
 
 const options = {
   method: 'GET',
   url: 'https://weatherapi-com.p.rapidapi.com/current.json',
   params: {q: '53.1,-0.13'},
   headers: {
-    'X-RapidAPI-Key': 'e106778c09msh13e2fb9682a4d94p1d03efjsn149de6591718',
+    'X-RapidAPI-Key': api_key,
     'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
   }
 };
